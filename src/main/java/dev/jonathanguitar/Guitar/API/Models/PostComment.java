4f1b59aba24fd4376df67a11b128a96e9df7025a
Jonathan Alvarado-Gomez <jonathan.alvaradogomez@revature.netgit config --global user.email jonathan.alvaradogomez@revature.netgit config --global user.email alvarado.jntn@gmail.comgit config --global user.name Jonathan>
2024-01-18 13:37:26 -0800

    PostComment model created.

---
+++ src/main/java/dev/jonathanguitar/Guitar/API/Models/PostComment.java
@@ -0,0 +1,21 @@
+package dev.jonathanguitar.Guitar.API.Models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "post_comments")
+@Data //This takes care of getter and setter methods.
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostComment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer commentId;
+    private Integer postId;
+    private Integer userId;
+    private String commentText;
+    private String commentDate;
+}
